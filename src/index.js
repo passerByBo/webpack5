ae17160ed9ca25bacd9a8b7d721e05ee1ba56621
shixiaobo <sxb5626@163.com>
2021-04-13 15:53:24 +0800

    zenghji

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
 import { output } from './main.js';
-=======
-import _ from 'lodash';
-import './style.css'
-import meinv from './meinv.png';
-import print from './print';
-function component() {
-    print();
-    const element = document.createElement('div');
->>>>>>> 6d1b754d4b653692a75828b7ea56656a6dd41f1d
+
 
 console.log(output);
